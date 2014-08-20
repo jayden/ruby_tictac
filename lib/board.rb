60f07e103d5bb1f9072ebdb490f59466b999f645
Jayden <sung.minsun@gmail.com>
2014-08-20 09:56:13 -0700

    Added spec and logic for determining winner

--- lib/board.rb
+++ lib/board.rb
@@ -4,6 +4,10 @@ class Board
 	include Marker
 	attr_accessor :spaces, :size
 
+	WINNING_PATTERNS = [0x1c0, 0x38, 0x7,
+						0x124, 0x92, 0x49,
+						0x111, 0x54]
+
 	def initialize(size=9)
 		@spaces = []
 		@size = size
@@ -17,5 +21,17 @@ class Board
 	def fill position, marker
 		raise Exception if spaces[position] != Marker::EMPTY
 		spaces[position] = marker
+	end
+
+	def isWinner? marker
+		pattern = 0x0
+
+		spaces.each_with_index do |mark, index|
+			pattern |= ( 1 << index ) if mark == marker
+		end
+
+		WINNING_PATTERNS.each do |winning_pattern|
+			return true if ( pattern & winning_pattern == winning_pattern )
+		end
 	end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -23,11 +23,11 @@ describe Board do
 		end
 	end
 
-	it "can fill spaces" do
-		expect(board).to respond_to :fill
-	end
-
 	context "filling spaces" do
+
+		it "can fill spaces" do
+			expect(board).to respond_to :fill
+		end
 
 		it "can fill a specific space" do
 			board.fill(0,x)
@@ -43,7 +43,71 @@ describe Board do
 
 	end
 
-	#PENDING
-	it "can determine if there's a winner"
+	context "there's a winner" do
+		it "finds winner in top row" do
+			board.fill(0,x)
+			board.fill(1,x)
+			board.fill(2,x)
+
+			expect(board.isWinner? x).to eql true
+		end
+
+		it "finds winner in middle row" do
+			board.fill(3,x)
+			board.fill(4,x)
+			board.fill(5,x)
+
+			expect(board.isWinner? x).to eql true
+		end
+
+		it "finds winner in bottom row" do
+			board.fill(6,x)
+			board.fill(7,x)
+			board.fill(8,x)
+
+			expect(board.isWinner? x).to eql true
+		end
+
+		it "finds winner in left column" do
+			board.fill(0,o)
+			board.fill(3,o)
+			board.fill(6,o)
+
+			expect(board.isWinner? o).to eql true
+		end
+
+		it "finds winner in middle column" do
+			board.fill(1,o)
+			board.fill(4,o)
+			board.fill(7,o)
+
+			expect(board.isWinner? o).to eql true
+		end	
+
+		it "finds winner in right column" do
+			board.fill(2,o)
+			board.fill(5,o)
+			board.fill(8,o)
+
+			expect(board.isWinner? o).to eql true
+		end	
+
+		it "finds winner in left diagonal" do
+			board.fill(0,x)
+			board.fill(4,x)
+			board.fill(8,x)
+
+			expect(board.isWinner? x).to eql true
+		end	
+
+		it "finds winner in right diagonal" do
+			board.fill(2,o)
+			board.fill(4,o)
+			board.fill(6,o)
+
+			expect(board.isWinner? o).to eql true
+		end			
+	end
+
 	it "can determine if there's a draw"
 end
