69bcf2276002494f9ffc7cf47cbc098e223aca22
jayden <jayden@users.noreply.github.com>
2014-08-24 20:34:19 -0700

    Update perfect_player.rb

--- lib/perfect_player.rb
+++ lib/perfect_player.rb
@@ -6,7 +6,7 @@ MIN_VALUE = -100000
 MAX_VALUE = 100000
 
 class PerfectPlayer < Player
-	def move
+def move
 		best_score_and_move = minimax(INITIAL_DEPTH, @marker)
 		@board.fill(best_score_and_move[1], @marker)
 	end
