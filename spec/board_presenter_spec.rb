916633879d98d94a8f82f1afef69c4bccd725d4c
Jayden <sung.minsun@gmail.com>
2014-09-02 09:09:34 -0700

    simplified tests

--- spec/board_presenter_spec.rb
+++ spec/board_presenter_spec.rb
@@ -3,6 +3,7 @@ require 'board'
 
 describe BoardPresenter do
 	let(:col_separator) {"\n-----------\n"}
+	let(:size) {9}
 
 	it "prints the board" do
 		board = Board.new
@@ -10,32 +11,22 @@ describe BoardPresenter do
 	end
 
 	it "prints a vertical separator at index 0" do
-		index = 0
-		size = 9
-		expect(BoardPresenter.write_separators(index, size)).to eql "|"
+		expect(BoardPresenter.write_separators(0, size)).to eql "|"
 	end
 
 	it "determines that index 0 is not the last column of first row" do
-		index = 0
-		size = 9
-		expect(BoardPresenter.last_col?(index, size)).to be false
+		expect(BoardPresenter.last_col?(0, size)).to be false
 	end
 
 	it "prints a horizontal separator at index 2" do
-		index = 2
-		size = 9
-		expect(BoardPresenter.write_separators(index, size)).to eql "#{col_separator}"
+		expect(BoardPresenter.write_separators(2, size)).to eql "#{col_separator}"
 	end
 
 	it "prints a horizontal separator of length 11 for board size 9" do
-		index = 2
-		size = 9
-		expect(BoardPresenter.print_line(index, size)).to eql col_separator
+		expect(BoardPresenter.print_line(2, size)).to eql col_separator
 	end
 
 	it "determines that index 2 is the last column of first row" do
-		index = 2
-		size = 9
-		expect(BoardPresenter.last_col?(index, size)).to be true
+		expect(BoardPresenter.last_col?(2, size)).to be true
 	end
 end
